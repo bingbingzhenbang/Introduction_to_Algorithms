9589fbe7ea95bc8e40f66041fedba2218c525c4c
bingbingbang <957438119@qq.com>
2016-10-19 21:46:27 +0800

    add new project

---
+++ Chapter33/ClosestPoints.cpp
@@ -0,0 +1,4 @@
+#include "Point.h"
+#include <vector>
+#include <algorithm>
+
---
+++ Chapter33/Point.h
@@ -0,0 +1,33 @@
+#ifndef POINT_H
+#define POINT_H
+
+#include <cmath>
+
+using std::sqrt;
+
+struct Point
+{
+	double m_x;
+	double m_y;
+};
+
+inline bool XLess(const Point &l, const Point &r)
+{
+	if (l.m_x == r.m_x)
+		return l.m_y < r.m_y;
+	return l.m_x < r.m_x;
+}
+
+inline bool YLess(const Point &l, const Point &r)
+{
+	if (l.m_y == r.m_y)
+		return l.m_x < r.m_x;
+	return l.m_y < r.m_y;
+}
+
+inline double Distance(const Point &l, const Point &r)
+{
+	return sqrt((l.m_x - r.m_x) * (l.m_x - r.m_x) + (l.m_y - r.m_y) * (l.m_y - r.m_y));
+}
+
+#endif // POINT_H
